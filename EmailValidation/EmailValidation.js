32f87fc7d91af978bb8031e969fb39f963b4a3da
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-02-03 19:41:37 +0530

    Email validation

--- EmailValidation/EmailValidation.js
+++ EmailValidation/EmailValidation.js
@@ -1,1 +1,16 @@
-console.log("Welcome to Email Validation");
+const emailRegex = RegExp("^[a-zA-Z][.@a-zA-Z]*$");
+
+function validateEmail(email){
+    if(emailRegex.test(email))
+        console.log(email + " : Valid EMAIL !!");
+    else
+        throw email + " : EMAIL is Invalid !";
+}
+
+try{
+    validateEmail("abc.xyz@bridgelabz.co.in");
+    validateEmail(".abc@abc.com");
+}
+catch(e){
+    console.error(e);
+}
